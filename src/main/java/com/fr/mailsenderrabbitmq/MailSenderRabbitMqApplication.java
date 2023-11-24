d0e87e84ca2c1eeb473cc4748d6a7a64fa03a722
RemxFr <epourexisterpro@gmail.com>
2023-11-24 10:56:20 +0100

    Initial commit

---
+++ src/main/java/com/fr/mailsenderrabbitmq/MailSenderRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.fr.mailsenderrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenderRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fr/mailsenderrabbitmq/MailSenderRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.mailsenderrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
